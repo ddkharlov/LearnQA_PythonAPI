dacabc2b03a1405eab15be403936a4bcd8c36a5d
kharlov <kharlov@lanit.ru>
2022-01-18 12:03:23 +0300

    Вынос запроса в метод

--- lib/my_requests.py
+++ lib/my_requests.py
@@ -1,22 +1,23 @@
 import requests
 
 
-class MyRequests():
+
+class MyRequests:
     @staticmethod
     def post(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
-        return MyRequests._send(url, data, headers, cookies, 'POST')
+            return MyRequests._send(url, data, headers, cookies, 'POST')
 
     @staticmethod
     def get(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
-        return MyRequests._send(url, data, headers, cookies, 'GET')
+            return MyRequests._send(url, data, headers, cookies, 'GET')
 
     @staticmethod
     def put(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
-        return MyRequests._send(url, data, headers, cookies, 'PUT')
+            return MyRequests._send(url, data, headers, cookies, 'PUT')
 
     @staticmethod
     def delete(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
-        return MyRequests._send(url, data, headers, cookies, 'DELETE')
+            return MyRequests._send(url, data, headers, cookies, 'DELETE')
 
     @staticmethod
     def _send(url: str, data: dict, headers: dict, cookies: dict, method: str):
@@ -28,15 +29,19 @@ class MyRequests():
         if cookies is None:
             cookies = {}
 
-            if method == 'GET':
-                response = requests.get(url, params=data, headers=headers, cookies=cookies)
-            elif method == 'POST':
-                response = requests.post(url, data=data, headers=headers, cookies=cookies)
-            elif method == 'PUT':
-                response = requests.put(url, data=data, headers=headers, cookies=cookies)
-            elif method == 'DELETE':
-                response = requests.delete(url, data=data, headers=headers, cookies=cookies)
-            else:
-                raise Exception(f"Bad HTTP method '{method}' was received")
 
-            return response
+
+        if method == "GET":
+            response = requests.get(url, params=data, headers=headers, cookies=cookies)
+        elif method == "POST":
+            response = requests.post(url, data=data, headers=headers, cookies=cookies)
+        elif method == "PUT":
+            response = requests.put(url, data=data, headers=headers, cookies=cookies)
+        elif method == "DELETE":
+            response = requests.delete(url, data=data, headers=headers, cookies=cookies)
+        else:
+            raise Exception(f"Bad HTTP request method '{method}' was received")
+
+
+        return response
+
--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,19 +1,19 @@
 import pytest
-from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from lib.my_requests import MyRequests
+
 
 class TestUserAuth(BaseCase):
-    exclude_params = [
-        ("no_cookie"),
-        ("no_token")
-    ]
+
+    exclude_params = ["no_cookie", "no_token"]
 
     def setup(self):
         data = {
             'email': 'vinkotov@example.com',
             'password': '1234'
-        }
+            }
+
         response1 = MyRequests.post("/user/login", data=data)
         self.auth_sid = self.get_cookie(response1, "auth_sid")
         self.token = self.get_header(response1, "x-csrf-token")
@@ -23,15 +23,15 @@ class TestUserAuth(BaseCase):
 
         response2 = MyRequests.get(
             "/user/auth",
-            headers={"x-csrf-token": self.token},
-            cookies={"auth_sid": self.auth_sid}
+            headers={'x-csrf-token': self.token},
+            cookies={'auth_sid': self.auth_sid}
         )
 
         Assertions.assert_json_value_by_name(
             response2,
             "user_id",
             self.user_id_from_auth_method,
-            "User id from auth method is not equal to user id from check method"
+            "User id from auth method is different with user_id from check method"
         )
 
     @pytest.mark.parametrize('condition', exclude_params)
@@ -52,5 +52,5 @@ class TestUserAuth(BaseCase):
             response2,
             "user_id",
             0,
-            f"User is authorized with condition {condition}"
+            f"User id is authorized with condition {condition}"
         )
