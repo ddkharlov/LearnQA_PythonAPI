d9ea660494aabda5b767f28aee56f49f0f0484f6
kharlov <kharlov@lanit.ru>
2022-01-18 11:30:26 +0300

    My_requests test_user_edit

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,5 +1,6 @@
 import json.decoder
 from requests import Response
+from datetime import datetime
 
 class BaseCase:
     def get_cookie(self, response: Response, cookie_name):
@@ -8,7 +9,7 @@ class BaseCase:
 
     def get_header(self, response: Response, headers_name):
         assert headers_name in response.headers, f"Cannot find cookie with name {headers_name} in the last response"
-        return response.cookies[headers_name]
+        return response.headers[headers_name]
 
     def get_json_value(self, response: Response, name):
         try:
@@ -19,5 +20,20 @@ class BaseCase:
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
 
         return response_as_dict[name]
+
+
+    def prepare_registration_data(self, email=None):
+        if email is None:
+            base_part = 'learnqa'
+            domain = 'example.com'
+            random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+            email = f"{base_part}{random_part}@{domain}"
+        return {
+            "password": "123",
+            "username": "learnq",
+            "firstName": "learnqa",
+            "lastName": "learnqa",
+            "email": email
+        }
 
 
--- lib/my_requests.py
+++ lib/my_requests.py
@@ -18,7 +18,6 @@ class MyRequests():
     def delete(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
         return MyRequests._send(url, data, headers, cookies, 'DELETE')
 
-
     @staticmethod
     def _send(url: str, data: dict, headers: dict, cookies: dict, method: str):
 
